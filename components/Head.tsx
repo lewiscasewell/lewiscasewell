5cc9ebb8cda56b9b4894ab879989c7a3f20523b4
Lewis Casewell <lewiscasewell@hotmail.co.uk>
2023-12-21 18:07:56 +0000

    pwa

--- components/Head.tsx
+++ components/Head.tsx
@@ -7,8 +7,9 @@ const Head = () => {
         name="description"
         content="SWE from the UK, mainly focused on React, React native, Typescript and Go"
       />
-      <meta name="theme-color" content="#18181b" />
+      <meta name="theme-color" content="#0a0a0a" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
     </NextHead>
   );
 };
--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const withPWA = require("next-pwa");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true, 
+})(nextConfig);
